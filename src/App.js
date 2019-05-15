9a6b70549dd36448bd4ff2196d43c3981160ef34
wonderdev7 <joekrit6@gmail.com>
2019-05-15 20:48:33 +0700

    click Title

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import Header from './Header'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Welcome
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "MyTitle",
+      count: 0
+    }
+  };
+ 
+  onClick = () => {
+    const { count } = this.state;
+    this.setState({
+      title: "MyTitle" + count,
+      count: count + 1
+    })
+  };
+
+  render() {
+    const { title } = this.state;
+    return (
+      <div className="App">
+        <Header title={title}/>
+        <button onClick={() => this.onClick()}>Click Here</button>
+      </div>
+    );
+  };
+};
 
 export default App;
