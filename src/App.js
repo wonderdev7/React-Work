40824dde1e8a146c704403b7b8deb67d28b844a2
wonderdev7 <joekrit6@gmail.com>
2019-05-17 16:26:16 +0700

    add header

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import React from 'react';
-import Header from './Header'
+import Header from './Header';
+import Main from './Main';
 import './App.css';
 
 class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: "MyTitle",
-      count: 0
-    }
-  };
- 
-  onClick = () => {
-    const { count } = this.state;
-    this.setState({
-      title: "MyTitle" + count,
-      count: count + 1
-    })
-  };
-
   render() {
-    const { title } = this.state;
     return (
       <div className="App">
-        <Header title={title}/>
-        <button onClick={() => this.onClick()}>Click Here</button>
+        <Header />
+        <Main />
       </div>
     );
-  };
-};
+  }
+}
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,51 @@
-import React from 'react'
+import React from 'react';
 
 export default class Header extends React.Component {
   render() {
-    const { title } = this.props;
     return (
-      <div>
-        {title}
-      </div>
-    )
-  };
-};
+      <header>
+        <div className="header-top">
+          <a className="mailbox" href="/">
+            <i className="fas fa-envelope"></i>
+            <span>AVAILABLE FOR FREELANCE</span>
+          </a>
+          <div className="icons">
+            <a className="icons__search" href="/">
+              <i className="fas fa-search"></i>
+            </a>
+            <a className="icons__download" href="/">
+              <i className="fas fa-download"></i>
+            </a>
+            <a className="icons__ellipsis" href="/">
+              <i className="fas fa-ellipsis-v"></i>
+            </a>
+          </div>
+        </div>
+        <div className="header-bottom">
+          <div className="logo">
+            SILITY
+          </div>
+          <div className="navbar">
+            <ul>
+              <li><a href="/">HOME</a></li>
+              <li><a href="/">ABOUT</a></li>
+              <li><a href="/">SKILL</a></li>
+              <li><a href="/">EXPERIENCE</a></li>
+              <li><a href="/">EDUCATION</a></li>
+              <li><a href="/">WORK</a></li>
+              <li><a href="/">BLOG</a></li>
+              <li><a href="/">CONTACT</a></li>
+            </ul>
+          </div>
+          <a className="burger" href="/">
+            Menu
+            <i className="fa fa-bars"></i>
+          </a>
+          <div className="btn-plus">
+            +
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
